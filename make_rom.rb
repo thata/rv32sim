de4ac8f56eac43efda18e2bd90c06c5bbf3dafbb
takashi hatakeyama <takashi.hatakeyama@gmail.com>
2021-12-12 18:44:37 +0900

    サンプルプログラムをフィボナッチにした

--- make_rom.rb
+++ make_rom.rb
@@ -4,13 +4,24 @@ require "./instructions"
 
 include Instructions
 
-# 命令メモリ
+# フィボナッチ数の第10項を求めプログラム
+# f(i): x1
+# f(i+1): x2
+# temp: x3
+# i: x4
+# n: x5
 rom = [
-  _addi(1, 0, 10), # x1 = 10
-  _addi(2, 0, 20), # x2 = 20
-  _addi(3, 0, 30), # x3 = 30
-  _add(4, 1, 2),   # x4 = x1 + x2
-  _add(5, 4, 3)    # x5 = x4 + x3
+  _addi(5, 0, 10),   # n = 10
+  _addi(4, 0, 0),    # i = 0
+  _addi(1, 0, 0),    # f(i) = 0
+  _addi(2, 0, 1),    # f(i+1) = 1
+  _beq(4, 5, 24),    # LOOP: goto BREAK if i == n
+  _add(3, 1, 2),     # temp = f(i) + f(i+1)
+  _addi(1, 2, 0),    # f(i) = f(i+1)
+  _addi(2, 3, 0),    # f(i+1) = temp
+  _addi(4, 4, 1),    # i = i + 1
+  _beq(0, 0, -20),   # goto LOOP
+  _nop,              # BREAK:
 ].pack("V*")
 
 print rom
