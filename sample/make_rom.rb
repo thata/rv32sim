16b57daca0496dd83834ddf0642735ab0a408108
takashi hatakeyama <takashi.hatakeyama@gmail.com>
2022-05-31 08:13:56 +0900

    フィボナッチの結果を0x100番地へ格納するようにした

--- sample/make_rom.rb
+++ sample/make_rom.rb
@@ -11,16 +11,19 @@ include Instructions
 # i: x4
 # n: x5
 rom = [
-  _addi(5, 0, 10),   # n = 10
-  _addi(4, 0, 0),    # i = 0
-  _addi(1, 0, 0),    # f(i) = 0
-  _addi(2, 0, 1),    # f(i+1) = 1
-  _beq(4, 5, 24),    # LOOP: goto BREAK if i == n
-  _add(3, 1, 2),     # temp = f(i) + f(i+1)
-  _addi(1, 2, 0),    # f(i) = f(i+1)
-  _addi(2, 3, 0),    # f(i+1) = temp
-  _addi(4, 4, 1),    # i = i + 1
-  _beq(0, 0, -20),   # goto LOOP
+  _addi(5, 0, 10),    # n = 10
+  _addi(4, 0, 0),     # i = 0
+  _addi(1, 0, 0),     # f(i) = 0
+  _addi(2, 0, 1),     # f(i+1) = 1
+  _beq(4, 5, 24),     # LOOP: goto BREAK if i == n
+  _add(3, 1, 2),      # temp = f(i) + f(i+1)
+  _addi(1, 2, 0),     # f(i) = f(i+1)
+  _addi(2, 3, 0),     # f(i+1) = temp
+  _addi(4, 4, 1),     # i = i + 1
+  _beq(0, 0, -20),    # goto LOOP
+  _addi(5, 0, 0x100), # addr = 0x100
+  _sw(5, 1, 0),       # M[addr] = f(i)
+  _nop,
   _nop,
   _nop,
   _nop,
