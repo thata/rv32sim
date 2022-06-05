5b13eaf4c102f15589f565f22807e8c6167a606d
takashi hatakeyama <takashi.hatakeyama@gmail.com>
2022-06-05 19:43:10 +0900

    4649〜

--- sample/make_rom.rb
+++ sample/make_rom.rb
@@ -1,28 +1,12 @@
 # ruby make_rom.rb > ./rom
 
-require "../instructions"
+require_relative "../instructions"
 
 include Instructions
 
-# フィボナッチ数の第10項を求めプログラム
-# f(i): x1
-# f(i+1): x2
-# temp: x3
-# i: x4
-# n: x5
 rom = [
-  _addi(5, 0, 10),    # n = 10
-  _addi(4, 0, 0),     # i = 0
-  _addi(1, 0, 0),     # f(i) = 0
-  _addi(2, 0, 1),     # f(i+1) = 1
-  _beq(4, 5, 24),     # LOOP: goto BREAK if i == n
-  _add(3, 1, 2),      # temp = f(i) + f(i+1)
-  _addi(1, 2, 0),     # f(i) = f(i+1)
-  _addi(2, 3, 0),     # f(i+1) = temp
-  _addi(4, 4, 1),     # i = i + 1
-  _beq(0, 0, -20),    # goto LOOP
-  _addi(5, 0, 0x100), # addr = 0x100
-  _sw(5, 1, 0),       # M[addr] = f(i)
+  _addi(1, 0, 0x46), # x1 = ヨロ(46)
+  _addi(2, 0, 0x49), # x2 = シク(49)
   _nop,
   _nop,
   _nop,
