640e8a93910f51d69b5b9f26af32b4389c391384
takashi hatakeyama <takashi.hatakeyama@gmail.com>
2022-06-05 21:23:48 +0900

    4649〜

--- sample/make_rom.rb
+++ sample/make_rom.rb
@@ -1,13 +1,13 @@
-# ruby make_rom.rb > ./rom
+# ruby sample/make_rom.rb > sample/4649.rom
 
 require_relative "../instructions"
 
 include Instructions
 
+# ここに命令を並べる
 rom = [
   _addi(1, 0, 0x46), # x1 = ヨロ(46)
   _addi(2, 0, 0x49), # x2 = シク(49)
-  _nop,
   _nop,
   _nop,
   _nop,
